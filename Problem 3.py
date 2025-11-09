c0add6ebc23965c000bc451c9a61a361e8fa6bc0
Md Oahadur Rahman Evu <mdoahadurrahmanevu@gmail.com>
2025-11-09 14:23:37 +0600

    new

--- Problem 3.py
+++
@@ -1,6 +0,0 @@
-
-print("Even Number 10 to 1")
-i=10
-while i>0:
-     print(i)
-     i-= 1
--- Problem1.py
+++
@@ -1,8 +0,0 @@
-
-n=int(input("Enter N number"))
-i = 1
-while i<= n:
-  sum=n*(n+1)/2
-  i += 1
-    
-print(sum) 
